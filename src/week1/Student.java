9cb904002b09655634a57dd357458b14d8b3f97f
buidangthutra <43228181+buidangthutra@users.noreply.github.com>
2018-09-18 22:22:45 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,16 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package week1;
 
+/**
+ *
+ * @author Admin
+ */
 public class Student {
-
-    // TODO: khai báo các thuộc tính cho Student
+     // TODO: khai báo các thuộc tính cho Student
+    private String name, id, group, email;
 
     // TODO: khai báo các phương thức getter, setter cho Student
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setId(String aId) {
+        id = aId;
+    }
+     public String getId() {
+        return id;
+    }
+    
+     public void setGroup(String aGroup) {
+         group = aGroup;
+     }
+     public String getGroup() {
+         return group;
+     }
+     
+     public void setEmail(String aEmail) {
+         email = aEmail;
+     }
+     public String getEmail() {
+         return email;
+     }
 
     /**
      * Constructor 1
      */
-    Student() {
+    public Student() {
         // TODO:
+        setName("Student");
+        setId("000");
+        setGroup("Int22041");
+        setEmail("uet@vnu.edu.vn");
+        
     }
 
     /**
@@ -19,20 +59,33 @@ public class Student {
      * @param sid
      * @param em
      */
-    Student(String n, String sid, String em) {
+    public Student(String n, String sid, String em) {
         // TODO:
+        this.name = n;
+        this.email = em;
+        this.id = sid;
+        setGroup("INT22041");
     }
 
     /**
      * Constructor 3
      * @param s
      */
-    Student(Student s) {
+    public Student(Student s) {
         // TODO:
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
+        
     }
 
-    String getInfo() {
+    void getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(group);
+        System.out.println(email);
+      //  return null; // xóa dòng này sau khi cài đặt
     }
 }
