b88eea079a2d3092c16847609b42c438fac8d014
buidangthutra <43228181+buidangthutra@users.noreply.github.com>
2018-09-27 10:09:05 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -8,12 +8,13 @@ public class Task1 {
             return gcd(b,a%b);
     }
 
-    public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        if(n < 0) return 0;
-        else if(n==0 || n==1) return n;
-        else return (fibonacci(n-1)+fibonacci(n-2));
-    }
+    public static int fibonacci (int n)
+	{
+		if ( n ==0 ) return 0;
+		if ( n ==1 ) return 1;
+		else return fibonacci(n-1)+fibonacci(n-2);
+		
+	}
    // public static void main(String[] args) {
    //     System.out.println("UCLN: "+gcd(5,2));
    //     System.out.println("Fibonacci: "+fibonacci(5));
