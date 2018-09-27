47cbffaa8eb0b4e68071d555b5a87b59b5a9f133
buidangthutra <43228181+buidangthutra@users.noreply.github.com>
2018-09-27 09:42:51 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,21 @@
 package week2.task1;
 
-public class Task1 {
 
-    public static int gcd(int a, int b) {
+public class Task1 {
+     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if(b==0) return a;
+            return gcd(b,a%b);
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        if(n < 0) return 0;
+        else if(n==0 || n==1) return n;
+        else return (fibonacci(n-1)+fibonacci(n-2));
     }
+   // public static void main(String[] args) {
+   //     System.out.println("UCLN: "+gcd(5,2));
+   //     System.out.println("Fibonacci: "+fibonacci(5));
+   // }
 }
