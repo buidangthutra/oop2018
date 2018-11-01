d9a7626b4778cd27d8bcfb5d98a4fd6ea308b479
buidangthutra <43228181+buidangthutra@users.noreply.github.com>
2018-11-01 11:35:57 +0700

    Update ExpressionTest.java

--- src/week7/task1/ExpressionTest.java
+++ src/week7/task1/ExpressionTest.java
@@ -7,11 +7,19 @@ package week7.task1;
  */
 public class ExpressionTest {
     public static void main(String[] args) {
+
        Square s = new Square(new Numeral(10));
        Multiplication m = new Multiplication(new Numeral(2),new Numeral(3));
        Subtration sup = new Subtration(s,new Numeral(1));
        Addition ad = new Addition(sup,m);
        Square re = new Square(ad);
         System.out.println(re.evaluate());
+        
+        try{
+            Division d = new Division(new Numeral(5),new Numeral(0));
+        }
+        catch(ArithmeticException e){
+            System.out.println("Loi chia cho 0");
+        }
     }
 }
