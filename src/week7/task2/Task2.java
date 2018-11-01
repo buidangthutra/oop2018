c505e817d88d97be48187cfb59a7743e204c332d
buidangthutra <43228181+buidangthutra@users.noreply.github.com>
2018-11-01 11:37:48 +0700

    Update Task2.java

--- src/week7/task2/Task2.java
+++ src/week7/task2/Task2.java
@@ -1,12 +1,15 @@
 package week7.task2;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Task2 {
     int a,b;
-    int[] c = new int[5];
-    public int Arithmetic(int x,int y) {
+    int[] c = new int[3];
+    int[] d;
+     public int Arithmetic(int x,int y) {
         a=x;
         b=y;
         if(y==0){
@@ -16,53 +19,61 @@ public class Task2 {
     }
     
     public void fileNotFoud() throws FileNotFoundException{
-        throw new FileNotFoundException();
+        File f = new File("abc");
+        FileReader a = new FileReader(f);
     }
 
     public void ArrayIndex() throws ArrayIndexOutOfBoundsException{
-        System.out.println(c[9]);
+        System.out.println(c[5]);
     }
     
     public void ClassCastException() throws ClassCastException{
-     throw new ClassCastException();
+     Object t = new Integer(4);
+     String h = (String) t;
     }
     
     public void NullPointerException() throws NullPointerException{
-        throw new NullPointerException();
+        System.out.println(d.length);
     }
     
     public void IOException() throws IOException{
-        throw new IOException();
+        File f = new File("abc");
+        FileReader a = new FileReader(f);
     }
     
     public static void main(String[] args) {
         Task2 a = new Task2();
         try{
-            a.fileNotFoud();
-            a.Arithmetic(5, 0);
-            a.ArrayIndex();
+            a.fileNotFoud();   
+        } catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+        try{
+             a.Arithmetic(5, 0);
+        }
+         catch(ArithmeticException e){
+            System.out.println(e);
+        }
+        try{
+             a.ArrayIndex();
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+        try{
             a.ClassCastException();
+        }catch(ClassCastException e){
+            System.out.println(e);
+        }
+        try{
             a.IOException();
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        try{
             a.NullPointerException();
-//        } catch(IOException e){
-//            
-//        }
-//        catch(NullPointerException e){
-//            
-//        }
-//        catch(ClassCastException e){
-//            
-//        }
-//        catch(ArrayIndexOutOfBoundsException e){
-//            
-//        }
-//        catch(ArithmeticException e){
-//            
+        }catch(NullPointerException e){
+            System.out.println(e);
         }
-        catch(Exception e){
-            
-        }
-        
     }
-    
 }
+
